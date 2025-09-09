7532f842e98b42ce7048aad88af1c2aeb12e296d
Alireza <alireza.khani.23231@gmail.com>
2025-09-09 23:14:13 +0330

    Update GitHub token for repository access

--- connections.py
+++ connections.py
@@ -3,7 +3,7 @@ from github import Github
 
 class Git:
     def __init__(self) -> None:
-        self.token = "??????????????????????????????????????????"
+        self.token = "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"
         self.repository = "alireza-py/result_data_connection"
         self.path = 'crypto_currencys.txt'
         self.object = Github(self.token)
